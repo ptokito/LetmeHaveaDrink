c24be29ecd525044bf0d2e066c7871e591cd4d70
Tim Okito <62076924+ptokito@users.noreply.github.com>
2020-12-14 00:53:29 -0500

    Add files via upload

---
+++ if_else_python.py
@@ -0,0 +1,24 @@
+print("Welcome to the Bar!")
+Age = int(input("How old are you? "))
+bill = 10
+Veteran = ("Are you a veteran? Y or N")
+ID = ()
+
+if Age >= 21 and Age <= 60:
+  print("You can Enter the bar")
+  bill -= 3 
+  print(f"Your final bill is ${bill}")
+if Age >= 65:
+    Veteran = input("Are you a veteran? Y or N. ")
+    if Veteran == 'Y':
+      if Veteran == 'N':
+        bill -= 10
+    print("Your entry fee is on the house! Thank you for your service")
+
+elif Age >= 18 and Age <= 21:
+  print("Go to Canada, you can have a drink there legally")
+
+else:
+  print("Go home and go to sleep")  
+
+
